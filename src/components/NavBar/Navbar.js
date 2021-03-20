ec8deb67bf43c867bc1f0e064d4c43fd342f50bd
ankitpegwal <ak0331486@gmail.com>
2021-03-21 01:05:23 +0530

    navbar completed

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,3 +1,5 @@
+import { faAddressCard, faBars, faBriefcase, faFileSignature, faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import logo from '../../aaa.png'
 const Navbar = () => {
@@ -6,22 +8,23 @@ const Navbar = () => {
      <div className="container">
         <a className="navbar-brand" href="#"><img src={logo} alt="logo.." className="logo" /></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+          {/* <span className="navbar-toggler-icon"></span> */}
+        <FontAwesomeIcon icon={faBars}  style={{color:"white"}} />
         </button>
       
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="#"> <FontAwesomeIcon icon={faHome} /> <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Work</a>
+              <a className="nav-link" href="#"><FontAwesomeIcon icon={faBriefcase} /></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
+              <a className="nav-link" href="#"><FontAwesomeIcon icon={faAddressCard} /></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
+              <a className="nav-link" href="#"><FontAwesomeIcon icon={faFileSignature} /></a>
             </li>
          
           </ul>
