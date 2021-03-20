151190210eb386d93f8ee2438c4e6966a6c2d580
ankitpegwal <ak0331486@gmail.com>
2021-03-20 22:06:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css'
+import Navbar from './components/NavBar/Navbar'
+
+const App = () => {
+  return (
+    <>
+      
+     <Navbar/>
+    </>
+  )
+}
+
+export default App
+
